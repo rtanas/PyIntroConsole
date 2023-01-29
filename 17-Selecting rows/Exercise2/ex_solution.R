e72811c6db9a912b79b26d3a377ac8fe84fc5fb4
ccrisc <78849337+ccrisc@users.noreply.github.com>
2023-01-29 21:38:00 +0100

    Update ex_solution.R

--- 17-Selecting rows/Exercise2/ex_solution.R
+++ 17-Selecting rows/Exercise2/ex_solution.R
@@ -1,1 +1,4 @@
+#change date format
+myData["TransDate"]  = pd.to_datetime(myData["TransDate"])
+#Select all purchases from 2010
 myData.loc[(myData["TransDate"]>pd.to_datetime("2009-12-31")) & (myData["TransDate"]<=pd.to_datetime("2010-12-31")), ]
