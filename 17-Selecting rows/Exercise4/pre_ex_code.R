8b324f317dab860152fc8dd9c67a73ce37efb9cc
ccrisc <78849337+ccrisc@users.noreply.github.com>
2023-01-29 17:50:19 +0100

    Update pre_ex_code.R

--- 17-Selecting rows/Exercise4/pre_ex_code.R
+++ 17-Selecting rows/Exercise4/pre_ex_code.R
@@ -1,2 +1,7 @@
-#load pandas
+#%% Import relevant modules
 import pandas as pd
+import numpy as np
+# Read in data
+myData=pd.read_csv("https://raw.githubusercontent.com/bachmannpatrick/Python-Class/master/data/transactions.csv")
+#fix the Date format
+myData["TransDate"]  = pd.to_datetime(myData["TransDate"])
