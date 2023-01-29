37109a6de1bf9d4e31714cf48c5acd4617f18195
ccrisc <78849337+ccrisc@users.noreply.github.com>
2023-01-29 17:24:12 +0100

    Update pre_ex_code.R

--- 09-Basic investigation of data/Exercise4/pre_ex_code.R
+++ 09-Basic investigation of data/Exercise4/pre_ex_code.R
@@ -1,1 +1,3 @@
 import pandas as pd
+myData=pd.read_csv("https://raw.githubusercontent.com/bachmannpatrick/Python-Class/master/data/transactions.csv")
+myData["TransDate"]  = pd.to_datetime(myData["TransDate"],format="%d.%m.%Y",utc=True)
